8e6f97c2d5cc9a6244c5257f6dc3d46c6da5bf0c
eepphhrreemm <eepphhrreemm@gmail.com>
2013-11-29 02:26:10 +0100

    Update translator.py

--- bin/translator.py
+++ bin/translator.py
@@ -1,26 +1,134 @@
-def getTranslation(sql_session, parsing_result):
-    #do something here
-    print('\n###Eprhems code here###')
+ """ 
+Graphical (?) interface for YADI2
+Last modify:
+	Marcello 12/11/2013
+"""
+# Create table first which has two or more attributs and the you have define it in the body ex if you have a table 
+#named B(x,y) colus make it like Q(x,y):-B(x,y). this will create view in the database
+
+import optparse
+from connect import getConnection
+from datalogQuery import datalogQuery
+from translator import getTranslation
+
+
+### GLOBAL VARIABLES ###
+sql_session = None
+
+
+
+### FUNCTIONS ###
+# This is for execution options from command line
+# In this example I've found, if you write 
+# "python gui.py --person Marcello" it returns "Hello Marcello"
+def main():
+    p = optparse.OptionParser()
+    p.add_option('--person', '-p', default="world")
+    options, arguments = p.parse_args()
+    print ('Hello %s' % options.person)
+
+
+
+#This function returns a connection object
+def connect():
+    hostname = 'localhost'
+    port = '5432'
+    username = 'postgres'
+    password = 'root'
+    dbname = 'adb'
+    connection_string = 'postgresql+psycopg2://' + username + ':' + password + '@' + hostname + ':' + port + '/' + dbname
+    print('\nDefault connection string is:\n%s\n' % connection_string)
+    answer = input("Would you like to use this setting? [Y/N] > ")
     
+    if answer.lower()=='y':
+        while True:
+            #insert parameters
+            hostname = input('\nPlease enter the PostgreSQL server name or IP address:\n> ')
+            port = input('\nPort:\n> ')
+            username = input('\nUsername:\n> ')
+            password = input('\nPassword:\n> ')
+            dbname = input('\nDatabase name:\n> ')
+           
+            if hostname and port and username and password and dbname:
+                connection_string = 'postgresql+psycopg2://' + username + ':' + password + '@' + hostname + ':' + port + '/' + dbname 
+                print('\nNew connection string:\n%s\n' % connection_string)
+                break
+            else:
+                print ('\nAll fields are mandatory.')
+  
+    global sql_session
+    sql_session = getConnection(connection_string)
+    
+    
+    
+#This function asks for a datalog query, parses it and returns the results 
+def datalog():
+    if not sql_session:
+        print ('\nPlease connect to a database.')
+        return
+        
+    while 1:
+        try:
+            string_to_parse = input('\nInsert Datalog query or rule.\n> ')
+        except EOFError:
+            break
+        if not string_to_parse: 
+            continue
+        
+        parsing_results = datalogQuery(string_to_parse)
+        getTranslation(sql_session, parsing_results)
+        ## PRINT FUNCTION HERE##
+        
+      
+    
+#This is a function we should implement later with an eventual query to test DB status...
+def status():
+    print('\n### Status goes here ###')
+    
+		
+#Also the help has to be done later
+def help():
+    print ('\n### Help goes here ###')
 
-# Ephrem Berhe
-# Transform string into columns name separatd with commas.
-def getTermList(TermList):
-    for term in TermList:
-        if(term !='_'):
-            Terms += term +","
-    return Terms
 
     
-def getConstantList(ConstantList):
-    if len(ConstantList)==0:
-        return ""
-    else: 
-        for cont in ConstantList:
-            Constats += cont +","
-            LastConst = " Where " + Constats	   
-    return LastConst
+### MAIN SECTION ###
+if __name__ == '__main__':
 
-    for rslt in results:
-        Query="CREATE VIEW "+rslt.predicate+" AS SELECT " + getTermList(rslt.terms) + " From "+ rslt.predicate + getConstantList(ConstantList)	
-        print (Query)	
+    #welcome message
+    print ('''
+---------------------------------
+       Welcome to YADI 2
+---------------------------------''')
+
+
+    #connection 
+    if not sql_session:
+        connect()
+     
+    
+    #user menu
+    options = {
+            '1' : connect,
+            '2' : status,
+            '3' : datalog,
+            '4' : help,
+            'q' : quit
+        }
+        
+    choice_text = '''
+What would you like to do?
+ 1. connect to a database
+ 2. check the status of a database
+ 3. a Datalog query
+ 4. I need help...
+ q. quit :(
+
+> '''
+  
+    while True:
+        choice = input(choice_text)
+        if choice in options:
+            options[choice]()
+        else:
+            print ('Option unavailable\n')
